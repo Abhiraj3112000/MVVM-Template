d0c8b47595a681646f5a002cf5da2976dd4bc81b
Abhiraj Chatterjee <64791997+Abhiraj3112000@users.noreply.github.com>
2025-06-28 02:06:49 +0530

    first commit

---
+++ MVVM_App_Template.swift
@@ -0,0 +1,18 @@
+//
+//  MVVM_App_Template.swift
+//  MVVM Example App
+//
+//  Created by Abhiraj Chatterjee on 28/06/25.
+//
+
+import SwiftUI
+
+struct MVVM_App_Template: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MVVM_App_Template()
+}
